befc3becefebd47b8f8c71bf8929e00acfee9957
Kimia <kimia.ehsani.dolati@iespoligonosur.org>
2022-01-13 16:52:41 +0100

    cine

--- ejercicio8/cinecito.php
+++ ejercicio8/cinecito.php
@@ -7,7 +7,6 @@ function __autoload($name)
 $cines[0] = new Cine(2, 'Razon 1', 'C0001A', 'Alicante', 'Calle 1', 1, new Dimensiones(10., 10., 10.), 10);
 $cines[1] = new Cine(4, 'Razon 2', 'C0002B', 'Alicante', 'Calle 2', 2, new Dimensiones(20., 20., 20.), 20);
 $cines[2] = new Cine(5, 'Razon 3', 'C0001C', 'Alicante', 'Calle 3', 3, new Dimensiones(30., 30., 30.), 30);
-
 $copia = clone $cines[1];
 $copia->dimensiones->alto = 40.;
 $copia->dimensiones->ancho = 50.;
